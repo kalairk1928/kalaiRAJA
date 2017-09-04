727e72a9293b559ab9de1a8a6ca2bf50b2e1a672
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2017-09-04 12:30:21 -0400

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <conio.h>
+
  
-int main(){
+void main(){
     char c;
     printf("Enter a Character: ");
     scanf("%c", &c);
@@ -11,6 +11,5 @@ int main(){
     } else {
         printf("%c is a Consonant\n", c);
     }
-    getch();
-    return 0;
+    
 }
