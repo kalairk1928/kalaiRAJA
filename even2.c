0a513b55f0ed2d8949d86745445f57ca0e22878d
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2017-09-05 16:40:06 +0530

    Create even2.c

---
+++ even2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int n,k =20,r=40;
+
+    clrscr();
+
+    printf("Print Odd Numbers in a given range kto r:\n");
+
+    for (n = k; n <= r; n++)
+
+        {
+
+               if (n % 2 == 1)
+
+                  printf ("%d ", n);
+
+         }
+
+                getch();
+
+}
+
+     
