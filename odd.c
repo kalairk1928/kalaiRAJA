2639f99a060746e6f7127be484ba4ca681428c7a
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2017-09-05 16:23:52 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ int k;
+ int r;
+ cout<<" enter number "<<endl;
+ cin>>k;
+ cout<<" enter number "<<endl;
+ cin>>r;
+ cout<<"*********************************"<<endl;
+ cout<<"even number"<<endl;
+ cout<<"**********************************"<<endl;
+ for(;k<=r;)
+ {
+ cout<<k<<endl;
+ k=k+2;
+ }
+}
