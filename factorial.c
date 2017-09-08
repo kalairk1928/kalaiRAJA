da7b33dc7dee1d535acf41323f2a1662d92efdd7
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2017-09-08 16:04:39 +0530

    Update and rename fact.c to factorial.c

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int k,r=1,s;
+ 
+  printf("Enter a number: ");
+  scanf("%d",&s);
+ 
+  for(k=1;k<=s;k++)
+      r=r*i;
+ 
+  printf("Factorial of %d is: %d",s,r);
+  return 0;
+}
